fded23f56792b0a0817c110d8fbf7d676d739a00
knnseleznov <knnseleznov@gmail.com>
2026-07-05 12:25:43 +0100

    chore: add logo & join all modules in main file & remove .gitkeep files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Finmate',
   description: 'Проект без стандартного шаблонного контента',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: 'cover',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import styles from './page.module.css';
+import { Header } from '@/widgets/header';
+import { Donut } from '@/widgets/donut';
+import { CategoriesList } from '@/widgets/categories-list';
+import { BottomNavigation } from '@/widgets/bottom-navigation';
+
 export default function Home() {
   return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Finmate</h1>
+    <main className={styles.container}>
+      <Header />
+
+      <section className={styles.chartSection}>
+        <Donut />
+      </section>
+
+      <CategoriesList />
+
+      <BottomNavigation />
     </main>
   );
 }
